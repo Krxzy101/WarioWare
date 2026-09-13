bfa2785ca8ef217d7185c1e09d8bec4704e46e5d
Aditya Chopra <adi.chopra2408@gmail.com>
2026-09-13 11:33:03 -0700

    Title Screen

---
+++ Scenes/title_scene.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+
+func _on_start_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/game_scene.tscn")
+
+
+func _on_settings_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_quit_pressed() -> void:
+	pass # Replace with function body.
