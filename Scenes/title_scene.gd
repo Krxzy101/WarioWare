1bf363a94befc062bad1dea98754660f611885c9
Aditya Chopra <adi.chopra2408@gmail.com>
2026-09-13 11:39:38 -0700

    Update title_scene.gd

--- Scenes/title_scene.gd
+++ Scenes/title_scene.gd
@@ -3,7 +3,7 @@ extends Node2D
 
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/game_scene.tscn")
-
+	 # This is quite literally the only "ai" code that will be used. My game is GD 4.4 and the tut is the newest version so I needed this start code
 
 func _on_settings_pressed() -> void:
 	pass # Replace with function body.
